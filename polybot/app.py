4424c36d617b6605ee84297bc0dfa0fb855c224e
ofekh <halabi1205@gmail.com>
2024-12-21 19:58:39 +0200

    update the workflow service deploy #7

--- polybot/app.py
+++ polybot/app.py
@@ -8,7 +8,6 @@ app = flask.Flask(__name__)
 TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
 TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
 
-
 @app.route('/', methods=['GET'])
 def index():
     return 'Ok'
